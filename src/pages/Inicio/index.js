8e877bc6488bafd6550c405b74eccde01f23077f
marciomazeu <mazeu911@gmail.com>
2024-02-09 21:28:01 -0500

    created the title component

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,12 +1,16 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Banner from "../../components/Banner";
+import Title from "../../components/Title";
 
 function Inicio(){
     return(
         <>
         <Header />
         <Banner imagem='banner'/>
+        <Title>
+            <h1>O seu lugar para guardar filmes!!</h1>
+        </Title>
         <Footer />
         </>
         
